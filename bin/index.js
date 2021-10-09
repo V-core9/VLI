6c395248f7be34c8ddd8f909d0ad137497fc22e9
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-10-09 14:53:59 +0200

    yea fixes

--- bin/index.js
+++ bin/index.js
@@ -9,23 +9,16 @@ var message = (typeof args.msg === "undefined") ? textNullVal : (`${args.msg}`);
 var type = (typeof args.type === "undefined") ? ((message !== textNullVal) ? "info" : "warn") : (`${args.type}`);
 
 
-
-
-
 if (typeof args.selftest !== "undefined") {
-  var testNumber = (args.selftest > 1) ? args.selftest : 10;
+  if (isNaN(args.selftest) || typeof args.selftest === "boolean") {
+    args.selftest = 10;
+  }
 
-  console.time("LOOP EXEC TIME")
-  if (message == "") message = `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-  
-  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-  
-  It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-`;
-  for (let i = 0; i < testNumber; i++) {
+
+  for (let i = 0; i < args.selftest; i++) {
     vLog.msg(vLog.generate.randomMessage(), vLog.generate.randomType());
   }
-  console.timeEnd("LOOP EXEC TIME")
+
 } else {
 
   vLog.msg(message, type);
