17829aa3aa08c72fc14c4e424f332977d7155437
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-10-09 14:45:23 +0200

    yea

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
 
-
+const textNullVal = "_!.NULL.!_";
+const args = require('args-parser')(process.argv);
 const vLog = require('../src/index.js');
 
-const args = require('args-parser')(process.argv);
 
-var message = "";
-message += (typeof args.msg === "undefined") ? "" : (`${args.msg}`);
-message += (typeof args.message !== "undefined") ? (`${args.message}`) : "";
-console.log(JSON.stringify(args))
-var type = "log";
-type = (typeof args.type === "undefined") ? "log" : (`${args.type}`);
+var message = (typeof args.msg === "undefined") ? textNullVal : (`${args.msg}`);
+var type = (typeof args.type === "undefined") ? ((message !== textNullVal) ? "info" : "warn") : (`${args.type}`);
+
+
+
 
 
 if (typeof args.selftest !== "undefined") {
@@ -24,8 +23,7 @@ if (typeof args.selftest !== "undefined") {
   It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 `;
   for (let i = 0; i < testNumber; i++) {
-    var type = vLog.generate.randomType();
-    vLog.msg(message, type);
+    vLog.msg(vLog.generate.randomMessage(), vLog.generate.randomType());
   }
   console.timeEnd("LOOP EXEC TIME")
 } else {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const config = require('./configs/main')
 var boxen = require('boxen');
 const chalk = require('chalk');
+var faker = require('faker');
+
 
 var profileExecTimes = (config.exec_times === true) ? () => { return true } : () => { return false };
 const messagesTXT = {
@@ -112,25 +114,24 @@ const vLog = {
       for (let i = 1; i <= helper; i++) {
         var positionToCut = (i === 1) ? (config.length_limit * i - 2) : (config.length_limit * i);
         logItem.msg = [logItem.msg.slice(0, positionToCut), "\n", logItem.msg.slice(positionToCut)].join('');
-
       }
     }
-    var styledMessage = `⌘  ${logItem.msg} \n⏰ ${logItem.ts}`;
+    var styledMessage = `\n⌘  ${logItem.msg} \n\n${chalk.gray("⏰ " + logItem.ts)}`;
 
     if (config.exec_times === true) styledMessage = `${styledMessage}\nExec: ${logItem.exec}`;
 
     switch (logItem.type) {
       case "log":
-        styledMessage = boxen(chalk.rgb(50, 150, 50)(styledMessage), { title: chalk.bgRgb(50, 150, 50)(` ⟁ Msg_UID : ${logItem.uid} `), padding: { top: 0, left: 2, right: 2, bottom: 0 } });
+        styledMessage = boxen(chalk.rgb(50, 150, 50)(styledMessage), { title: chalk.bold.white.bgRgb(50, 150, 50)(` ⟁ Msg_UID : ${logItem.uid} `), padding: { top: 0, left: 2, right: 2, bottom: 0 } });
         break;
       case "info":
-        styledMessage = boxen(chalk.rgb(50, 150, 210)(styledMessage), { title: chalk.bgRgb(10, 100, 200)(` ⌆ Info_UID : ${logItem.uid} `), padding: { top: 0, left: 2, right: 2, bottom: 0 } });
+        styledMessage = boxen(chalk.rgb(50, 150, 210)(styledMessage), { title: chalk.bold.white.bgRgb(10, 100, 200)(` ⌆ Info_UID : ${logItem.uid} `), padding: { top: 0, left: 2, right: 2, bottom: 0 } });
         break;
       case "warn":
-        styledMessage = boxen(chalk.rgb(225, 100, 50)(styledMessage), { title: chalk.bgRgb(225, 100, 50)(` ⌬ Warn_UID : ${logItem.uid} `), padding: { top: 0, left: 2, right: 2, bottom: 0 } });
+        styledMessage = boxen(chalk.rgb(225, 100, 50)(styledMessage), { title: chalk.bold.white.bgRgb(225, 100, 50)(` ⌬ Warn_UID : ${logItem.uid} `), padding: { top: 0, left: 2, right: 2, bottom: 0 } });
         break;
       case "error":
-        styledMessage = boxen(chalk.rgb(250, 50, 50)(styledMessage), { title: chalk.bgRgb(180, 60, 60)(` ⮽ Error_UID : ${logItem.uid} `), padding: { top: 0, left: 2, right: 2, bottom: 0 } });
+        styledMessage = boxen(chalk.rgb(250, 50, 50)(styledMessage), { title: chalk.bold.white.bgRgb(180, 60, 60)(` ⮽ Error_UID : ${logItem.uid} `), padding: { top: 0, left: 2, right: 2, bottom: 0 } });
         break;
 
       default:
@@ -145,6 +146,9 @@ const vLog = {
     randomType() {
       var randomNumber = getRandomInt(4) + 1;
       return (randomNumber % 2 === 0) ? ((randomNumber % 4 === 0) ? "log" : "info") : ((randomNumber % 3 === 0) ? "warn" : "error")
+    },
+    randomMessage() {
+      return faker.lorem.sentences(getRandomInt(3) + 1) + "\n\n" + faker.lorem.sentences(getRandomInt(2) + 1);
     }
   }
 }
